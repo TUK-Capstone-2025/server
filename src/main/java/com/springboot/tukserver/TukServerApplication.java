efdd990250b47d485906bdbc897dfb9dc057e7e9
mdmmdm26 <minsh0826@gmail.com>
2025-01-04 01:28:32 +0900

    first commit

---
+++ src/main/java/com/springboot/tukserver/TukServerApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.tukserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TukServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TukServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/springboot/tukserver/TukServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.tukserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TukServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
